78373a4139d4ffa8c722eabfa32a8dcd48c4adbf
nyan9 <rynnaing@gmail.com>
2021-03-29 21:30:28 -0400

    add user_auth form and routes

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Connect Friends!</h1>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
