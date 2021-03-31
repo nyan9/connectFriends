3ed61d85925407c649310329a605eb9aac1990ec
nyan9 <rynnaing@gmail.com>
2021-03-31 15:27:00 -0400

    change game mode route

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch } from "react-router-dom";
-import {Route} from "react-router-dom";
-import Game from '../components/game';
-import Chatbox from '../components/chatbox/chatbox'
-import ModePage from './mode/mode_page'
+import { Switch, Route } from "react-router-dom";
+import Game from "../components/game";
+import Chatbox from "../components/chatbox/chatbox";
+import ModePage from "./mode/mode_page";
 import NavBarContainer from "./nav/navbar_container";
 // import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
@@ -14,7 +13,7 @@ const App = () => (
   <div>
     <NavBarContainer />
     <Switch>
-      <ProtectedRoute exact path="/" component={ModePage} />
+      <Route exact path="/" component={ModePage} />
       <ProtectedRoute exact path="/play" component={Game} />
     </Switch>
   </div>
