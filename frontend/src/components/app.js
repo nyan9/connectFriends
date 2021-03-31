991bccae420dcb2fa4dd4ce71f715fdd657d9e83
Kenny Li <kenny.wj.li@gmail.com>
2021-03-31 10:14:19 -0400

    errors

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,14 +2,13 @@ import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 import {Route} from "react-router-dom";
-
+import Game from '../components/game';
 import Chatbox from '../components/chatbox/chatbox'
 
-import Game from "./game";
 import NavBarContainer from "./nav/navbar_container";
-import ModePage from "./mode/mode_page";
-
-
+// import MainPage from "./main/main_page";
+import LoginFormContainer from "./session/login_form_container";
+import SignupFormContainer from "./session/signup_form_container";
 
 const App = () => (
   <div>
@@ -20,9 +19,6 @@ const App = () => (
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
        <Route path="/play" component={Game} />
        <Route path="/chat" component={Chatbox} />
-      <ProtectedRoute exact path="/play" component={Game} />
-      <AuthRoute exact path="/" component={ModePage} />
-      <AuthRoute exact path="/play" component={Game} />
     </Switch>
   </div>
 );
