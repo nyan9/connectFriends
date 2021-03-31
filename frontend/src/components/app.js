4b9c80afc1ca489d3abb63cd2483cb7df581d4cc
Kenny Li <kenny.wj.li@gmail.com>
2021-03-31 16:49:20 -0400

    heroku bug

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,6 +5,7 @@ import {Route} from "react-router-dom";
 import Game from '../components/game';
 import Chatbox from '../components/chatbox/chatbox'
 import ModePage from './mode/mode_page'
+import Leaderboard from './leaderboard/leaderboard'
 import NavBarContainer from "./nav/navbar_container";
 // import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
@@ -16,6 +17,7 @@ const App = () => (
     <Switch>
       <ProtectedRoute exact path="/" component={ModePage} />
       <ProtectedRoute exact path="/play" component={Game} />
+      <ProtectedRoute exact path="/leaderboard" component={Leaderboard}/>
     </Switch>
   </div>
 );
