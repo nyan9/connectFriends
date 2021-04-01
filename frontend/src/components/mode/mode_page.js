4629bbf8b766f4e08803d5a820d9ad814528753b
nyan9 <rynnaing@gmail.com>
2021-04-01 15:45:44 -0400

    pass to Charm

--- frontend/src/components/mode/mode_page.js
+++ frontend/src/components/mode/mode_page.js
@@ -1,38 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {connect} from "react-redux";
-import {resetChat} from "../../actions/chat_actions"
+import { connect } from "react-redux";
+import { resetChat } from "../../actions/chat_actions";
+import "./mode_page.scss";
 
 class ModePage extends React.Component {
-
-  componentDidMount(){
-    this.props.resetChat()
+  componentDidMount() {
+    this.props.resetChat();
   }
   render() {
     return (
-      <>
-        <div>
-          <Link to="/cpu"> Single Player(AI) </Link>
-        </div>
-        <br />
-        <div>
-          <Link to="/local"> Local 2 Player (Local)</Link>
-        </div>
-        <br />
-        <div>
-          <Link to="/online">Online Multiplayer</Link>
-        </div>
-      </>
+      <section class="card-list">
+        <article class="card">
+          <header class="card-header">
+            <h2>Single Player(AI)</h2>
+          </header>
+
+          <div class="card-logo">
+            <h2>LOGO HERE</h2>
+          </div>
+
+          <div class="card-button">
+            <Link to="/cpu"> Play </Link>
+          </div>
+        </article>
+
+        <article class="card">
+          <header class="card-header">
+            <h2>Local 2 Player (Local)</h2>
+          </header>
+
+          <div class="card-logo">
+            <h2>LOGO HERE</h2>
+          </div>
+
+          <div class="card-button">
+            <Link to="/local"> Play </Link>
+          </div>
+        </article>
+
+        <article class="card">
+          <header class="card-header">
+            <h2>Online Multiplayer</h2>
+          </header>
+
+          <div class="card-logo">
+            <h2>LOGO HERE</h2>
+          </div>
+
+          <div class="card-button">
+            <Link to="/online"> Play </Link>
+          </div>
+        </article>
+      </section>
     );
   }
 }
 
-
-
-const mDTP = dispatch => {
+const mDTP = (dispatch) => {
   return {
-    resetChat: () => dispatch(resetChat())
-  }
-}
+    resetChat: () => dispatch(resetChat()),
+  };
+};
 
-export default connect(null, mDTP)(ModePage)
+export default connect(null, mDTP)(ModePage);
