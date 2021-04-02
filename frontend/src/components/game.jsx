4beae2e1c3d4f8a3d9c48b8b4f82231230de9562
Kenny Li <kenny.wj.li@gmail.com>
2021-04-01 21:51:31 -0400

    fixed board bug

--- frontend/src/components/game.jsx
+++ frontend/src/components/game.jsx
@@ -91,8 +91,8 @@ class Game extends React.Component {
         let winloss = (
             <div>
                 win:{this.state.win}
-                    loss:{this.state.loss}
-                    tie:{this.state.tieCount}
+                loss:{this.state.loss}
+                tie:{this.state.tieCount}
             </div>
         )
 
