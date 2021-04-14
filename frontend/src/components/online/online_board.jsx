ea793d76de7678676f386ee235d793f36d7fc7a0
John <johncheung1997@gmail.com>
2021-04-14 14:45:15 -0400

    reduce interval time

--- frontend/src/components/online/online_board.jsx
+++ frontend/src/components/online/online_board.jsx
@@ -53,7 +53,7 @@ export default class OnlineBoard extends React.Component {
         console.log("send pos: ", lastPos)
         this.props.socket.emit("send pos", lastPos)
       };
-    }, 1000);
+    }, 10);
   }
 
   updateBoard(lastPos_and_color) {
