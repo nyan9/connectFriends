7c7f359be9436602c5e5bb3a551cc5447c22162f
John <johncheung1997@gmail.com>
2021-04-14 12:11:47 -0400

    made copy of online logic

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const io = require("socket.io")(server, {
   },
 });
 const { Chat } = require("./models/Chat");
-const Online = require("./frontend/online_logic");
+const Online = require("./online_logic");
 
 const connect = mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
